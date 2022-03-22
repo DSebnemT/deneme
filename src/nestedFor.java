3b337cfb936997333ff3aece479f18ee8addab67
Didem Toygar <didemt@Didems-Air.lan>
2022-03-22 14:14:38 -0500

    try

---
+++ src/nestedFor.java
@@ -0,0 +1,37 @@
+public class nestedFor {
+
+    public static void main(String[] args) {
+       /* String a = "cat-cheetah-dog-cat";
+        int countcat=0;
+        int countdog=0;
+        for(int i=0;i<a.length()-2;i++){
+            if(a.substring(i,i+3).equals("cat")){
+                countcat++;
+            }
+            if (a.substring(i, i + 3).equals("dog")) {
+                countdog++;
+            }
+        }
+
+        if(countcat==countdog){
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+
+
+        }
+
+        */
+
+       /* String name = "Jennet";
+                for (int i = 0 ; i<= name.length()-1 ; i++ ){
+
+                    System.out.println(name.charAt(i));
+                }
+
+
+    }
+
+        */
+
+}
